48d8913b84724789fc2e7cac4693858172683218
ib2953 <ib2953@gmail.com>
2023-05-17 01:52:22 +0100

    Top level import fix

--- fnde/__init__.py
+++ fnde/__init__.py
@@ -1,3 +1,3 @@
-from models.fnde import FNDE
-from models.fno import FNO
-from models.node import NODE, ODEIntegrator
+from fnde.models.fnde import FNDE
+from fnde.models.fno import FNO
+from fnde.models.node import NODE, ODEIntegrator
